0cbee481fd996d08011bd58910ef1b3ab024b94c
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-07-06 18:25:05 +0000

    Move logging proxy object to new file, plash_logger.py
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@173 e2ebe5e9-4d0c-0410-8560-844d39661dae

---
+++ python/plash_logger.py
@@ -0,0 +1,32 @@
+
+import plash_marshal
+
+indent = ['>']
+
+# Logging proxy object
+class logger(plash_marshal.Pyobj_marshal):
+    def __init__(self, x1): self.x = x1
+    def cap_call(self, args):
+        print indent[0], "call",
+        try:
+            print plash_marshal.unpack(args)
+        except:
+            print args, "exception:",
+            traceback.print_exc()
+        try:
+            i = indent[0]
+            indent[0] = i + '  '
+            try:
+                r = self.x.cap_call(args)
+            finally:
+                indent[0] = i
+        except:
+            print indent[0], "Exception in cap_call:",
+            traceback.print_exc()
+            return
+        print indent[0], "->",
+        try:
+            print plash_marshal.unpack(r)
+        except:
+            print r
+        return r
--- python/test3.py
+++ python/test3.py
@@ -11,59 +11,7 @@ import plash_marshal as m
 import plash_namespace as ns
 import plash_process
 from plash_process import Process_spec
-
-
-# Logging proxy object
-class logger(plash_marshal.Pyobj_marshal):
-    def __init__(self, x1): self.x = x1
-    def cap_call(self, args):
-        try:
-            print "call", plash_marshal.unpack(args)
-        except:
-            print args, "exception:",
-            traceback.print_exc()
-        try:
-            r = self.x.cap_call(args)
-        except:
-            print "Exception in cap_call:",
-            traceback.print_exc()
-            return
-        try:
-            print "->", plash_marshal.unpack(r)
-        except Exception, e:
-            print r
-        return r
-
-indent = ['>']
-
-class logger_indent(plash_marshal.Pyobj_marshal):
-    def __init__(self, x1): self.x = x1
-    def cap_call(self, args):
-        print indent[0], "call",
-        try:
-            print plash_marshal.unpack(args)
-        except:
-            print args, "exception:",
-            traceback.print_exc()
-        try:
-            i = indent[0]
-            indent[0] = i + '  '
-            try:
-                r = self.x.cap_call(args)
-            finally:
-                indent[0] = i
-        except:
-            print indent[0], "Exception in cap_call:",
-            traceback.print_exc()
-            return
-        print indent[0], "->",
-        try:
-            print plash_marshal.unpack(r)
-        except:
-            print r
-        return r
-
-logger = logger_indent
+from plash_logger import logger
 
 
 class Test_dir(plash_marshal.Pyobj_demarshal):

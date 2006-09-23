96c1d28672d14e970bb76b4ba0413f76a4f22fc6
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-09-23 10:04:58 +0000

    Add list of modules to install and metadata
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@217 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,15 @@
 from distutils.core import setup, Extension
 
 setup(name="plash", version="1.0",
+      author='Mark Seaborn',
+      author_email='mrs@mythic-beasts.com',
+      url='http://plash.beasts.org',
+      py_modules=['plash_env',
+                  'plash_logger',
+                  'plash_marshal',
+                  'plash_methods', # auto-generated
+                  'plash_namespace',
+                  'plash_process'],
       ext_modules=[Extension("plash",
                              include_dirs = ["../src", "../gensrc"],
                              library_dirs = ["../obj"],

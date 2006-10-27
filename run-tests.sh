8556a378b12ab43fdc495bf41836ca9817e71ccd
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-10-27 18:17:28 +0000

    Split off separate run-uninstalled.sh script from run-tests.sh
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@261 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- run-tests.sh
+++ run-tests.sh
@@ -9,21 +9,4 @@ mkdir -p lib
  mkdir -p lib &&
  python setup.py install --install-platlib=lib)
 
-# Create wrapper script for pola-run that sets the library path, etc.
-DIR=`pwd`
-(
- echo "#!/bin/sh"
- echo "exec $DIR/bin/pola-run -fl \$PLASH_LIBRARY_DIR \"\$@\""
-) > lib/pola-run
-chmod +x lib/pola-run
-
-export PLASH_SANDBOX_PROG=$DIR/tests/wrapper.sh
-export PLASH_P_SANDBOX_PROG=$DIR/shobj/ld.so
-export PLASH_LDSO_PATH=$DIR/shobj/ld.so
-export PLASH_LIBRARY_DIR=$DIR/lib
-
-export PATH=$DIR/lib:$DIR/bin:$PATH
-export PYTHONPATH=$DIR/python:$DIR/python/lib:$PYTHONPATH
-
-cd tests
-./run-tests.pl
+./run-uninstalled.sh sh -c 'cd tests && ./run-tests.pl'
---
+++ run-uninstalled.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# = 0 ]; then
+  echo "Usage: $0 prog args..."
+  exit 1
+fi
+
+
+DIR=`pwd`
+
+# Create wrapper script for pola-run that sets the library path, etc.
+(
+ echo "#!/bin/sh"
+ echo "exec $DIR/bin/pola-run -fl \$PLASH_LIBRARY_DIR \"\$@\""
+) > lib/pola-run
+chmod +x lib/pola-run
+
+export PLASH_SANDBOX_PROG=$DIR/tests/wrapper.sh
+export PLASH_P_SANDBOX_PROG=$DIR/shobj/ld.so
+export PLASH_LDSO_PATH=$DIR/shobj/ld.so
+export PLASH_LIBRARY_DIR=$DIR/lib
+
+export PATH=$DIR/lib:$DIR/bin:$PATH
+export PYTHONPATH=$DIR/python:$DIR/python/lib:$PYTHONPATH
+
+exec "$@"

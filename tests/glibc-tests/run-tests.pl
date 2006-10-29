31585804724441b1b03d9982a7d4e0a7a6b68ac6
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-10-29 21:38:18 +0000

    Change output format to use with summarise.pl Record time taken for each test Try to substitute GLIBC_BUILD_DIR in test args Don't run slow tests; add test-list-slow
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@273 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- tests/glibc-tests/run-tests.pl
+++ tests/glibc-tests/run-tests.pl
@@ -1,32 +1,54 @@
 #!/usr/bin/perl -w
 
 use IO::File;
+use Time::HiRes qw(gettimeofday);
 
 $|=1; # switch off output buffering
 
 
+my $tests = {};
 foreach my $test (split(/\s+/, slurp('test-list'))) {
+  $tests->{$test} = 1;
+}
+foreach my $test (split(/\s+/, slurp('test-list-slow'))) {
+  delete $tests->{$test};
+}
+
+
+foreach my $test (sort(keys(%$tests))) {
   my $result;
-  print "$test:\t";
+  print "\n--- running $test\n";
 
   my $input = "$test.input";
   if(!-e $input) { $input = '/dev/null' }
   
-  if(!-e $test) { $result = MISSING }
-  elsif(join(' ', slurp($input), slurp("$test.env"), slurp("$test.args"))
-	=~ /GLIBC_BUILD_DIR/) {
-    $result = SKIPPED
+  if(!-e $test) {
+    $result = 'missing';
   }
   else {
+    my $env = slurp("$test.env");
+    my $args = slurp("$test.args");
+    
+    my $dir = "../../glibc-2.3.6-objs";
+    $env =~ s/GLIBC_BUILD_DIR/$dir/g;
+    $args =~ s/GLIBC_BUILD_DIR/$dir/g;
+    write_file('tmp-env', $env);
+    write_file('tmp-args', $args);
+    
     # LOCPATH=../../glibc/localedata
     # NB. LC_ALL=C is used in glibc's Rules file
-    my $rc =
-      system("env LC_ALL=C `cat $test.env` $test `cat $test.args` <$input >$test.out 2>&1");
-    if($rc == 0) { $result = OK }
-    else { $result = FAIL }
+    my $cmd = "env LC_ALL=C `cat tmp-env` $test `cat tmp-args` <$input >$test.out 2>&1";
+    
+    print "$cmd\n";
+    my $time0 = gettimeofday();
+    my $rc = system($cmd);
+    my $time1 = gettimeofday();
+    if($rc == 0) { $result = 'ok' }
+    else { $result = 'failed' }
+    printf "-- %s took %fs\n", $test, $time1 - $time0;
   }
 
-  print "$result\n";
+  print "** $test $result\n";
 }
 
 
@@ -38,4 +60,12 @@ sub slurp {
   my $d = join('', <$f>);
   $f->close();
   $d
+}
+
+sub write_file {
+  my ($file, $data) = @_;
+  my $f = IO::File->new($file, 'w');
+  if(!defined $f) { die "Can't open `$file' for writing" }
+  print $f $data;
+  $f->close();
 }

c66b4865d5aeb78ec354f648a640d36a9ac866bd
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-11-06 16:33:31 +0000

    Be careful not to add an empty entry to LD_LIBRARY_PATH
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@284 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- tests/wrapper.sh
+++ tests/wrapper.sh
@@ -2,5 +2,10 @@
 
 # This script is for use with the PLASH_SANDBOX_PROG environment variable
 
-export LD_LIBRARY_PATH=$PLASH_LIBRARY_DIR:$LD_LIBRARY_PATH
+if test "$LD_LIBRARY_PATH" = ""; then
+  export LD_LIBRARY_PATH=$PLASH_LIBRARY_DIR
+else
+  export LD_LIBRARY_PATH=$PLASH_LIBRARY_DIR:$LD_LIBRARY_PATH
+fi
+
 exec "$PLASH_LDSO_PATH" "$@"

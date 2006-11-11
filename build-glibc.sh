07d3dadc209a629bb0bb3d29486187683a834bec
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-11-11 15:56:51 +0000

    build-glibc.sh: Add support for building glibc 2.3.6 debian/rules: Change to use build-glibc.sh
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@297 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- build-glibc.sh
+++ build-glibc.sh
@@ -15,35 +15,64 @@ GLIBC_DIR=glibc-$GLIBC_VERSION
 GLIBC_BUILD=glibc-$GLIBC_VERSION-objs
 GLIBC_ARCHIVE=glibc-$GLIBC_VERSION.tar.bz2
 
+# Only needed for glibc <2.4
+LINUXTHREADS_ARCHIVE=glibc-linuxthreads-$GLIBC_VERSION.tar.bz2
+
 
 unpack () {
+  # Check that files are present
+  FAIL=0
   if [ ! -e $GLIBC_ARCHIVE ]; then
     echo "$0: File $GLIBC_ARCHIVE not present"
     echo "$0: Try downloading ftp://ftp.gnu.org/gnu/glibc/$GLIBC_ARCHIVE"
+    FAIL=1
+  fi
+  if [ $GLIBC_VERSION = 2.3.6 ] &&
+     [ ! -e $LINUXTHREADS_ARCHIVE ]; then
+    echo "$0: File $LINUXTHREADS_ARCHIVE not present"
+    echo "$0: Try downloading ftp://ftp.gnu.org/gnu/glibc/$LINUXTHREADS_ARCHIVE"
+    FAIL=1
+  fi
+  if [ $FAIL -ne 0 ]; then
     exit 1
   fi
+
+  # Unpack
   mkdir -p tmp
   bzip2 -cd $GLIBC_ARCHIVE | tar -C tmp -xf -
+  if [ $GLIBC_VERSION = 2.3.6 ]; then
+    bzip2 -cd $LINUXTHREADS_ARCHIVE | tar -C tmp/$GLIBC_DIR -xf -
+    # Apply patch
+    patch -p1 -d tmp/$GLIBC_DIR <patches/glibc-2005-10-04.diff
+  fi
+  # Move into place
   mv -v tmp/$GLIBC_DIR .
 }
 
 configure () {
   mkdir -p $GLIBC_BUILD
 
-  # The following have been removed for glibc 2.4:
-  # --without-__thread
-  # --enable-add-ons=linuxthreads
-  # CC=gcc-3.3
-
   # Use --disable-profile to save disc space
   # -g1 saves a lot of space
-  cd $GLIBC_BUILD && \
-  ../$GLIBC_DIR/configure \
+
+  if [ $GLIBC_VERSION = 2.3.6 ]; then
+    cd $GLIBC_BUILD && \
+    ../$GLIBC_DIR/configure \
+	CC=gcc-3.3 \
+	--prefix=/usr \
+	--enable-add-ons=linuxthreads \
+	--without-selinux --enable-kernel=2.2.0 \
+	--with-tls --without-__thread \
+	--disable-profile CFLAGS="-pipe -fstrict-aliasing -g1 -O3"
+  else
+    cd $GLIBC_BUILD && \
+    ../$GLIBC_DIR/configure \
 	CC=gcc-4.0 \
 	--prefix=/usr \
 	--without-selinux --enable-kernel=2.2.0 \
 	--with-tls \
 	--disable-profile CFLAGS="-pipe -fstrict-aliasing -g1 -O3"
+  fi
 }
 
 build () {

31c4a22bdcc891ba013d456535d2de32e0027704
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-11-19 19:52:41 +0000

    Add comment (partly taken from make-link-def.pl)
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@344 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- src/export-renames.pl
+++ src/export-renames.pl
@@ -9,6 +9,26 @@ if(scalar(@ARGV) == 1 && $ARGV[0] eq '--rtld') {
 }
 
 
+# Read glibc's generated abi-versions.h in order to get a mapping of
+# version symbols, e.g.
+# #define VERSION_libc_GLIBC_2_1_1   GLIBC_2.1.1
+#
+# Some non-i386 architectures don't provide all version symbols.
+# For example, x86-64 glibc doesn't provide GLIBC_2.0, because that
+# predated glibc being ported to x86-64.
+# GLIBC_2.0 gets mapped to GLIBC_2.2.5 instead.
+#
+# When building glibc normally, the preprocessor performs this mapping
+# for us.  The concatenation "VERSION_##lib##_##version" gives us (for
+# example) "VERSION_libc_GLIBC_2_1_1" (see glibc's shlib-compat.h).
+# This would normally be expanded to "GLIBC_2.1.1".  However, in our
+# case, it is then concatenated again:
+# "exportver_##name##_version_##version".  The preprocessor does not
+# expand at every step, so the expansion to "GLIBC_2.1.1" does not
+# happen.  Even if the expansion did happen, it could not give a
+# single token containing dots.  This all means that we have to do the
+# expansion outside of the preprocessor.
+
 my $glibc_dir = `. src/config.sh && echo \$GLIBC`;
 chomp($glibc_dir);
 if($glibc_dir eq '') { die }

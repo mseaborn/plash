10c83c126197da285a89a2893b7d64fc9ed4e596
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-12-07 15:26:24 +0000

    Extend to package glibc 2.4 as well as 2.3.6
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@380 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- glibc-source-package/make.sh
+++ glibc-source-package/make.sh
@@ -2,15 +2,29 @@
 
 set -e
 
-BASE=glibc-source-2.3.6-2.3.6
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <glibc-version>"
+  exit 1
+fi
+
+VERSION=$1
+BASE=glibc-source-$VERSION-$VERSION
 
 mkdir $BASE
 mkdir $BASE.orig
 
-cp -al glibc-2.3.6.tar.bz2 glibc-linuxthreads-2.3.6.tar.bz2 $BASE.orig
+case $VERSION in
+  2.3.6)
+    cp -al glibc-$VERSION.tar.bz2 glibc-linuxthreads-$VERSION.tar.bz2 $BASE.orig
+    ;;
+  2.4)
+    cp -al glibc-$VERSION.tar.bz2 $BASE.orig
+    ;;
+esac
 
+mkdir $BASE/debian
 # A trick for copying a directory while excluding ".svn":
-tar -cf - debian --exclude .svn | tar -xf - -C $BASE
+tar -cf - -C debian-$VERSION . --exclude .svn | tar -xf - -C $BASE/debian
 
 dpkg-source -b -i".*tar.bz2" -I.svn $BASE $BASE.orig
 
@@ -19,7 +33,7 @@ rm -rf $BASE
 
 
 # Now unpack the source package and build it
-dpkg-source -x glibc-source-2.3.6_2.3.6-1.dsc
+dpkg-source -x glibc-source-${VERSION}_$VERSION-1.dsc
 (cd $BASE && dpkg-buildpackage -rfakeroot -D)
 # Again:
 rm -rf $BASE

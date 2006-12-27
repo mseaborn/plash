9ee559e24fc61ba5b885d427025b85804e9646c4
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2006-12-27 15:25:10 +0000

    Add some symbols to preload-libc.so's import list Add comments
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@416 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- src/libc-preload-import.c
+++ src/libc-preload-import.c
@@ -23,14 +23,41 @@
 #include <dlfcn.h>
 
 
+/* This is used in the LD_PRELOADable build of Plash's libc code,
+   which is used only for testing purposes.
+
+   The shared object needs to be able to both import and export
+   symbols of the same name.  e.g. It wraps the close() function.  We
+   need to look up symbols at run time using dlsym() in order to do
+   this.
+
+   We use gcc's __builtin_apply() facility so that:
+   1) We can make the imported functions appear as functions not
+   function pointers.  This means we don't have to rebuild Plash's
+   libc code for linking into preload-libc.so.
+   2) We don't have to specify the functions' prototypes.
+
+   __builtin_apply() might disappear from gcc in the future.  May have
+   to use libffi instead in that case. */
+
+
 #define IMPORT(name) \
 void name() \
 { \
   void *sym = dlsym(RTLD_NEXT, #name); \
+  /* 100 is the maximum size of the arguments on the stack. */ \
   __builtin_return(__builtin_apply(sym, __builtin_apply_args(), 100)); \
 }
 
 IMPORT(__fxstat);
 IMPORT(close);
+IMPORT(dup);
+IMPORT(dup2);
 IMPORT(fork);
 IMPORT(execve);
+IMPORT(bind);
+IMPORT(connect);
+IMPORT(getsockname);
+IMPORT(getsockopt);
+IMPORT(getuid);
+IMPORT(getgid);

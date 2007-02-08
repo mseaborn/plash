f55a9a89789a50c66e62d0cd5f7f1631596c379b
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2007-02-08 23:26:48 +0000

    Story1:  * Add Python script for running deb preinst/postinst scripts.    Sets up COW dir properly, unlike the init.sh script it replaces.
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@453 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- packaging/pkg.py
+++ packaging/pkg.py
@@ -7,9 +7,19 @@ def get_block(file):
     lines = []
     while True:
         line = file.readline()
+        if line == "" and len(lines) == 0:
+            raise EOFError()
         if line == "" or line == "\n":
             return lines
         lines.append(line)
+
+def file_blocks(file):
+    """Returns an iterable of lists of lines."""
+    try:
+        while True:
+            yield get_block(file)
+    except EOFError:
+        pass
 
 def block_fields(lines):
     d = {}
---
+++ packaging/run-deb-inst-scripts.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+"""Run preinst and postinst scripts for Debian packages."""
+
+import os
+import sys
+
+import plash_core
+import plash.namespace as ns
+from plash.process import Process_spec
+import plash.mainloop
+
+import pkg
+
+
+class File_namespace:
+
+    def __init__(self):
+        self._root = plash.env.get_root_dir()
+        self._cwd = ns.resolve_dir(self._root, os.getcwd())
+
+    def get_obj(self, pathname):
+        return ns.resolve_obj(self._root, pathname,
+                              cwd=self._cwd, nofollow=False)
+
+
+unpack_cache = os.path.realpath("unpack-cache")
+
+cwd = os.getcwd()
+my_root = File_namespace()
+os.chdir(cwd)
+
+
+def init_package(app_dir, package, control_script, args):
+    proc = Process_spec()
+    proc.root_node = ns.make_node()
+    proc.caps["conn_maker"] = ns.conn_maker
+
+    root_dir = ns.make_cow_dir(my_root.get_obj(
+                                 os.path.join(app_dir, "unpacked")),
+                               my_root.get_obj(
+                                 os.path.join(app_dir, "write_layer")))
+    ns.attach_at_path(proc.root_node, "/", root_dir)
+    ns.resolve_populate(my_root._root, proc.root_node,
+                        "/usr/lib/plash/lib", my_root._cwd)
+
+    script_path = os.path.join(unpack_cache, package,
+                               "control", control_script)
+
+    if os.path.exists(script_path):
+        ns.attach_at_path(proc.root_node, "/script",
+                          my_root.get_obj(script_path))
+
+        proc.caps["fs_op"] = \
+            plash_core.make_fs_op(ns.dir_of_node(proc.root_node))
+        proc.caps["fs_op"].fsop_chdir("/")
+        proc.setcmd(*(["/bin/sh", "-c", 'exec "$@"', "-",
+                       "/script"] + args))
+        pid = proc.spawn()
+        plash.mainloop.run_server()
+    else:
+        print "  no %s" % control_script
+
+def init_packages(app_dir):
+    fh = open(os.path.join(app_dir, "package-list"), "r")
+    try:
+        for block in pkg.file_blocks(fh):
+            info = pkg.block_fields(block)
+            package = "%s_%s" % (info["package"], info["version"])
+            print package
+            init_package(app_dir, package, "preinst", ["install"])
+            init_package(app_dir, package, "postinst", ["configure"])
+    finally:
+        fh.close()
+
+def main(args):
+    if len(args) != 1:
+        print "Usage: %s <app-dir>" % sys.argv[0]
+    else:
+        init_packages(args[0])
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

0f7155328ada8bcac20fae184c2cade349a9fdf4
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2007-03-03 22:20:32 +0000

    Story1:  * Re-implement plash-pkg-install in Python (remove Perl implementation).
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@478 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- packaging/plash_pkg.py
+++ packaging/plash_pkg.py
@@ -8,6 +8,20 @@ class BadControlLineError(Exception):
     pass
 
 
+def read_file(filename):
+    fh = open(filename, "r")
+    try:
+        return fh.read()
+    finally:
+        fh.close()
+
+def write_file(filename, data):
+    fh = open(filename, "w")
+    try:
+        fh.write(data)
+    finally:
+        fh.close()
+
 def join_with_slash(a, b):
     return "%s/%s" % (a.rstrip("/"), b.lstrip("/"))
 

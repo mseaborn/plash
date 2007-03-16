4ec9c458fdff98b78f0e2b22f15596007977863e
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2007-03-16 20:31:27 +0000

    Story1:  * Move cache directories to ~/.cache/plash-pkg instead of putting them    into the current working directory.  This can be overridden by setting    PLASH_PKG_CACHE_DIR.
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@506 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- python/plash_pkg/config.py
+++ python/plash_pkg/config.py
@@ -10,8 +10,17 @@ def ensure_dir_exists(dir_path):
 def get_arch():
     return "i386"
 
+def get_cache_dir():
+    if "PLASH_PKG_CACHE_DIR" in os.environ:
+        return os.environ["PLASH_PKG_CACHE_DIR"]
+    else:
+        # Default is ~/.cache/plash-pkg
+        home = os.environ["HOME"]
+        cache = ensure_dir_exists(os.path.join(home, ".cache"))
+        return ensure_dir_exists(os.path.join(cache, "plash-pkg"))
+
 def get_package_list_cache_dir():
-    return ensure_dir_exists("package-lists")
+    return ensure_dir_exists(os.path.join(get_cache_dir(), "package-lists"))
 
 def get_package_list_combined():
     return os.path.join(get_package_list_cache_dir(),
@@ -19,12 +28,12 @@ def get_package_list_combined():
 
 def get_deb_cache_dir():
     """Directory where downloaded .deb files are stored."""
-    return ensure_dir_exists("cache")
+    return ensure_dir_exists(os.path.join(get_cache_dir(), "deb-cache"))
 
 def get_unpack_cache_dir():
     """Directory where unpacked copies of packages are stored."""
-    return ensure_dir_exists("unpack-cache")
+    return ensure_dir_exists(os.path.join(get_cache_dir(), "unpack-cache"))
 
 def get_desktop_files_dir():
     """Directory where generated .desktop files are written."""
-    return ensure_dir_exists("desktop-files")
+    return ensure_dir_exists(os.path.join(get_cache_dir(), "desktop-files"))

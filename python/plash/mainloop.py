f0fb39c06342507b4adf0bdf66888db8881435e1
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2007-03-18 20:15:55 +0000

    * In plash.mainloop, switch to using gobject's interface to the    event loop rather than gtk's interface, so that this can be run    without an X display.
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@511 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- python/plash/mainloop.py
+++ python/plash/mainloop.py
@@ -1,17 +1,17 @@
 
 import plash_core
 
-_use_gtk = False
+_use_glib = False
 
 def use_gtk_mainloop():
+    use_glib_mainloop()
+
+def use_glib_mainloop():
     """Enable use of Gtk/Glib's top-level event loop, instead of
     Plash's built-in event loop.
     """
-    global _use_gtk
-    _use_gtk = True
-
-def use_glib_mainloop():
-    use_gtk_mainloop()
+    global _use_glib
+    _use_glib = True
 
 
 _forwarders = 0
@@ -29,9 +29,9 @@ def run_server():
     """Run the top-level loop.  Returns when this process is no longer
     exporting any object references.
     """
-    if _use_gtk:
-        import gtk
+    if _use_glib:
+        import gobject
         while plash_core.cap_server_exporting():
-            gtk.main_iteration()
+            gobject.main_context_default().iteration()
     else:
         plash_core.run_server()

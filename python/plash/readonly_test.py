7a7c6f75e503271af1ccff726ea58b0a8858e2c5
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2007-12-27 20:04:54 +0000

    StoryTest1: readonly_test: Extend to cover directories and symlinks
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@678 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- python/plash/readonly_test.py
+++ python/plash/readonly_test.py
@@ -45,6 +45,14 @@ def make_mock_fd():
     return plash.filedesc.wrap_fd(os.open("/dev/null", os.O_RDONLY, 0))
 
 
+class ReadOnlyProxyTestBase(unittest.TestCase):
+
+    def assert_rejected(self, func):
+        # FIXME: want a different exception
+        self.assertRaises(plash.marshal.UnmarshalError, func)
+        self.assertEquals(self.orig_obj._calls, [])
+
+
 class MockFile(plash.marshal.Pyobj_demarshal):
 
     def __init__(self):
@@ -63,43 +71,139 @@ class MockFile(plash.marshal.Pyobj_demarshal):
         return make_mock_fd()
 
 
-class ReadOnlyProxyTest(unittest.TestCase):
+class ReadOnlyFileProxyTest(ReadOnlyProxyTestBase):
 
     def setUp(self):
-        self.file_obj = MockFile()
-        self.read_only = plash.namespace.make_read_only_proxy(self.file_obj)
+        self.orig_obj = MockFile()
+        self.read_only = plash.namespace.make_read_only_proxy(self.orig_obj)
 
     def test_open_for_read_succeeds(self):
         self.read_only.file_open(os.O_RDONLY)
-        self.assertEquals(self.file_obj._calls,
+        self.assertEquals(self.orig_obj._calls,
                           [("file_open", os.O_RDONLY)])
 
     def test_open_for_write_fails(self):
-        # FIXME: want a different exception
-        self.assertRaises(plash.marshal.UnmarshalError,
-                          lambda: self.read_only.file_open(os.O_WRONLY))
-        self.assertEquals(self.file_obj._calls, [])
+        self.assert_rejected(lambda: self.read_only.file_open(os.O_WRONLY))
 
     def test_type(self):
         self.assertEquals(self.read_only.fsobj_type(), plash.marshal.OBJT_FILE)
-        self.assertEquals(self.file_obj._calls, [("fsobj_type",)])
+        self.assertEquals(self.orig_obj._calls, [("fsobj_type",)])
 
     def test_stat(self):
         self.assertEquals(self.read_only.fsobj_stat(), mock_stat_info)
-        self.assertEquals(self.file_obj._calls, [("fsobj_stat",)])
+        self.assertEquals(self.orig_obj._calls, [("fsobj_stat",)])
 
     def test_chmod(self):
-        self.assertRaises(plash.marshal.UnmarshalError,
-                          lambda: self.read_only.fsobj_chmod(0777))
+        self.assert_rejected(lambda: self.read_only.fsobj_chmod(0777))
 
     def test_utimes(self):
-        self.assertRaises(plash.marshal.UnmarshalError,
-                          lambda: self.read_only.fsobj_utimes(1, 2, 3, 4))
+        self.assert_rejected(lambda: self.read_only.fsobj_utimes(1, 2, 3, 4))
 
     def test_socket_connect(self):
-        self.assertRaises(plash.marshal.UnmarshalError,
-                          lambda: self.read_only.file_socket_connect(
-                                      make_mock_fd()))
+        self.assert_rejected(
+            lambda: self.read_only.file_socket_connect(make_mock_fd()))
+
+
+class MockDir(plash.marshal.Pyobj_demarshal):
+
+    def __init__(self):
+        self._calls = []
+
+    @call_logger
+    def fsobj_type(self):
+        return plash.marshal.OBJT_DIR
+
+    @call_logger
+    def fsobj_stat(self):
+        return mock_stat_info
+
+    @call_logger
+    def dir_list(self):
+        return [{"inode": 1, "type": 0, "name": "foo"}]
+
+
+class ReadOnlyDirProxyTest(ReadOnlyProxyTestBase):
+
+    def setUp(self):
+        self.orig_obj = MockDir()
+        self.read_only = plash.namespace.make_read_only_proxy(self.orig_obj)
+
+    def test_type(self):
+        self.assertEquals(self.read_only.fsobj_type(), plash.marshal.OBJT_DIR)
+        self.assertEquals(self.orig_obj._calls, [("fsobj_type",)])
+
+    def test_stat(self):
+        self.assertEquals(self.read_only.fsobj_stat(), mock_stat_info)
+        self.assertEquals(self.orig_obj._calls, [("fsobj_stat",)])
+
+    def test_dir_list(self):
+        self.assertEquals(self.read_only.dir_list(),
+                          [{"inode": 1, "type": 0, "name": "foo"}])
+        self.assertEquals(self.orig_obj._calls, [("dir_list",)])
+
+    def test_create_file(self):
+        self.assert_rejected(
+            lambda: self.read_only.dir_create_file(os.O_WRONLY, 0666,
+                                                   "leafname"))
+
+    def test_mkdir(self):
+        self.assert_rejected(
+            lambda: self.read_only.dir_mkdir(0666, "leafname"))
+
+    def test_symlink(self):
+        self.assert_rejected(
+            lambda: self.read_only.dir_symlink("dest_path", "leafname"))
+
+    def test_rename(self):
+        self.assert_rejected(
+            lambda: self.read_only.dir_rename("from_leafname",
+                                              self.read_only,
+                                              "to_leafname"))
+
+    def test_link(self):
+        self.assert_rejected(
+            lambda: self.read_only.dir_link("from_leafname",
+                                            self.read_only,
+                                            "to_leafname"))
+
+    def test_unlink(self):
+        self.assert_rejected(lambda: self.read_only.dir_unlink("leafname"))
+
+    def test_rmdir(self):
+        self.assert_rejected(lambda: self.read_only.dir_rmdir("leafname"))
+
+    def test_socket_bind(self):
+        self.assert_rejected(
+            lambda: self.read_only.dir_socket_bind("leafname", make_mock_fd()))
+
+
+class MockSymlink(plash.marshal.Pyobj_demarshal):
+
+    def __init__(self):
+        self._calls = []
+
+    @call_logger
+    def fsobj_type(self):
+        return plash.marshal.OBJT_SYMLINK
+
+    @call_logger
+    def symlink_readlink(self):
+        return "link_dest"
+
+
+class ReadOnlySymlinkProxyTest(ReadOnlyProxyTestBase):
+
+    def setUp(self):
+        self.orig_obj = MockSymlink()
+        self.read_only = plash.namespace.make_read_only_proxy(self.orig_obj)
+
+    def test_type(self):
+        self.assertEquals(self.read_only.fsobj_type(),
+                          plash.marshal.OBJT_SYMLINK)
+        self.assertEquals(self.orig_obj._calls, [("fsobj_type",)])
+
+    def test_readlink(self):
+        self.assertEquals(self.read_only.symlink_readlink(), "link_dest")
 
 
 if __name__ == "__main__":

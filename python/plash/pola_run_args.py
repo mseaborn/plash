5d47c5ff7d37c9fddcb0e41d0baf63ce1babe040
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2008-04-22 20:26:17 +0000

    PlashIssues/PolaRunEqualsSyntax: Allow use of "=" in more pola-run options: -f=file, --prog=name, --cwd=dir, --env=key=value, --fd=number, --log-file=file, etc. The C implementation had allowed these, but the later Python implementation had regressed.  Refactored option handling.
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@748 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- python/plash/pola_run_args.py
+++ python/plash/pola_run_args.py
@@ -73,6 +73,42 @@ def get_arg(args, name):
     return args.pop(0)
 
 
+class OptionWithSingleArg(object):
+
+    def __init__(self, func):
+        self._func = func
+
+    def handle_option(self, name, trailing, args):
+        if trailing == "":
+            self._func(get_arg(args, name))
+        elif trailing.startswith("="):
+            self._func(trailing[1:])
+        else:
+            raise BadArgException("Unrecognised argument: %s%s" %
+                                  (name, trailing))
+
+
+class OptionTrailing(object):
+
+    def __init__(self, func):
+        self._func = func
+
+    def handle_option(self, name, trailing, args):
+        self._func(trailing, args)
+
+
+class OptionNoTrailing(object):
+
+    def __init__(self, func):
+        self._func = func
+
+    def handle_option(self, name, trailing, args):
+        if trailing != "":
+            raise BadArgException("Unrecognised argument: %s%s" %
+                                  (name, trailing))
+        self._func(args)
+
+
 class ProcessSetup(object):
     """Processes the command line arguments accepted by pola-run.
     Grant a process a subset of the caller's authority."""
@@ -87,6 +123,30 @@ class ProcessSetup(object):
         if parent_environ is None:
             parent_environ = os.environ
         self._parent_environ = parent_environ
+        self._options = {
+            "f": OptionTrailing(self.bind_at_same),
+            "t": OptionTrailing(self.bind_at),
+            "a": OptionWithSingleArg(self.add_arg),
+            "e": OptionNoTrailing(self.command_and_args),
+            "B": OptionNoTrailing(self.default_env),
+            "h": OptionNoTrailing(self.help),
+            "prog": OptionWithSingleArg(self.set_executable),
+            "cwd": OptionWithSingleArg(self.set_cwd),
+            "no-cwd": OptionNoTrailing(self.unset_cwd),
+            "copy-cwd": OptionNoTrailing(self.copy_cwd),
+            "env": OptionWithSingleArg(self.add_environ_var),
+            "fd": OptionWithSingleArg(self.add_fd),
+            "x11": OptionNoTrailing(self.grant_x11_access),
+            "net": OptionNoTrailing(self.grant_network_access),
+            "tmp": OptionNoTrailing(self.grant_tmp),
+            "tmpdir": OptionWithSingleArg(self.grant_tmp_dir),
+            "log": OptionNoTrailing(self.enable_logging),
+            "log-file": OptionWithSingleArg(self.log_to_file),
+            "debug": OptionNoTrailing(self.debug),
+            "pet-name": OptionWithSingleArg(self.set_pet_name),
+            "powerbox": OptionNoTrailing(self.enable_powerbox),
+            "help": OptionNoTrailing(self.help),
+            }
 
     # -f<flags> <filename>
     def bind_at_same(self, flags_arg, args):
@@ -119,9 +179,8 @@ class ProcessSetup(object):
 
     # -a <string>
     # Append string to argument list.
-    def add_arg(self, args):
-        x = get_arg(args, '-a')
-        self.proc.args.append(x)
+    def add_arg(self, arg):
+        self.proc.args.append(arg)
 
     # -e [<command>] <args...>
     # This option consumes the remaining arguments in the list.
@@ -143,14 +202,13 @@ class ProcessSetup(object):
                      "-fl,objrw", "/dev/null",
                      ])
 
-    def set_executable(self, args):
+    def set_executable(self, arg):
         if self.proc.cmd != None:
             raise BadArgException, "--prog argument used multiple times"
-        self.proc.cmd = get_arg(args, '--prog')
+        self.proc.cmd = arg
 
-    def set_cwd(self, args):
-        path = args.pop(0)
-        self.cwd = ns.resolve_dir(self.caller_root, path, cwd=self.cwd)
+    def set_cwd(self, dir_path):
+        self.cwd = ns.resolve_dir(self.caller_root, dir_path, cwd=self.cwd)
 
     def unset_cwd(self, args):
         self.cwd = None
@@ -160,13 +218,10 @@ class ProcessSetup(object):
 
     # --env NAME=VALUE
     # Sets an environment variable.
-    def add_environ_var(self, args):
-        x = args.pop(0)
-        index = x.find('=')
-        if index == -1:
-            raise BadArgException, "Bad --env parameter: %s" % x
-        name = x[:index]
-        value = x[index+1:]
+    def add_environ_var(self, arg):
+        if "=" not in arg:
+            raise BadArgException("Bad --env parameter: %s" % arg)
+        name, value = arg.split("=", 1)
         self.proc.env[name] = value
 
     def grant_proxy_terminal_access(self):
@@ -180,8 +235,8 @@ class ProcessSetup(object):
 
     # -fd <fd-number>
     # Add file descriptor.
-    def add_fd(self, args):
-        fd = int(get_arg(args, "--fd"))
+    def add_fd(self, arg):
+        fd = int(arg)
         self.proc.fds[fd] = plash_core.wrap_fd(os.dup(fd))
 
     # --x11: Grant access to X Window System displays
@@ -231,8 +286,7 @@ class ProcessSetup(object):
     def grant_tmp(self, args):
         self.proc.get_namespace().attach_at_path("/tmp", self._make_temp_dir())
 
-    def grant_tmp_dir(self, args):
-        dest_path = get_arg(args, "--tmpdir")
+    def grant_tmp_dir(self, dest_path):
         self.proc.get_namespace().attach_at_path(dest_path,
                                                  self._make_temp_dir())
 
@@ -240,8 +294,7 @@ class ProcessSetup(object):
         fd = plash_core.wrap_fd(os.dup(2))
         self.proc.logger = ns.make_log_from_fd(fd)
 
-    def log_to_file(self, args):
-        log_filename = args.pop(0)
+    def log_to_file(self, log_filename):
         fd = plash_core.wrap_fd(
             os.open(log_filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC))
         self.proc.logger = ns.make_log_from_fd(fd)
@@ -249,8 +302,8 @@ class ProcessSetup(object):
     def debug(self, args):
         raise BadArgException, "not implemented"
 
-    def set_pet_name(self, args):
-        self.pet_name = get_arg(args, "--pet-name")
+    def set_pet_name(self, name):
+        self.pet_name = name
 
     def enable_powerbox(self, args):
         self.powerbox = True
@@ -258,58 +311,31 @@ class ProcessSetup(object):
     def help(self, args):
         raise UsageException()
 
-    options = {}
-
     def handle_arg(self, args):
         arg = args.pop(0)
-        if arg == "" or arg[0] != "-":
-            raise BadArgException, "Unknown argument: %s" % arg
-        elif arg[1] in self.options:
-            method_name, allow_trailing = self.options[arg[1]]
-            if allow_trailing:
-                getattr(self, method_name)(arg[2:], args)
+        if not arg.startswith("-"):
+            raise BadArgException("Unknown argument: %s" % arg)
+        if arg[1] in self._options:
+            handler = self._options[arg[1]]
+            handler.handle_option(arg[:2], arg[2:], args)
+            return
+        if arg.startswith("--"):
+            if "=" in arg:
+                arg_name, rest = arg.split("=", 1)
+                trailing = "=" + rest
             else:
-                getattr(self, method_name)(args)
-        elif arg[1] == "-" and arg[2:] in self.options:
-            method_name, allow_trailing = self.options[arg[2:]]
-            assert not allow_trailing
-            getattr(self, method_name)(args)
-        else:
-            raise BadArgException, "Unrecognised argument: %s" % arg
+                arg_name = arg
+                trailing = ""
+            handler = self._options.get(arg_name[2:], None)
+            if handler is not None:
+                handler.handle_option(arg_name, trailing, args)
+                return
+        raise BadArgException("Unrecognised argument: %s" % arg)
 
     def handle_args(self, args):
         args = args[:]
         while len(args) > 0:
             self.handle_arg(args)
-
-
-def add_option(name, method_name):
-    ProcessSetup.options[name] = (method_name, False)
-def add_option_t(name, method_name):
-    ProcessSetup.options[name] = (method_name, True)
-
-add_option_t("f", "bind_at_same")
-add_option_t("t", "bind_at")
-add_option("a", "add_arg")
-add_option("e", "command_and_args")
-add_option("B", "default_env")
-add_option("h", "help")
-add_option("prog", "set_executable")
-add_option("cwd", "set_cwd")
-add_option("no-cwd", "unset_cwd")
-add_option("copy-cwd", "copy_cwd")
-add_option("env", "add_environ_var")
-add_option("fd", "add_fd")
-add_option("x11", "grant_x11_access")
-add_option("net", "grant_network_access")
-add_option("tmp", "grant_tmp")
-add_option("tmpdir", "grant_tmp_dir")
-add_option("log", "enable_logging")
-add_option("log-file", "log_to_file")
-add_option("debug", "debug")
-add_option("pet-name", "set_pet_name")
-add_option("powerbox", "enable_powerbox")
-add_option("help", "help")
 
 
 def set_fake_uids(proc):
--- python/plash/pola_run_args_test.py
+++ python/plash/pola_run_args_test.py
@@ -67,6 +67,122 @@ class TestEnvironment(unittest.TestCase):
         write_file(filename, data)
         return filename
 
+    def test_option_a(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["-a", "bumblebee"])
+        self.assertEquals(proc.args, ["bumblebee"])
+        setup.handle_args(["-a=nickel"])
+        self.assertEquals(proc.args, ["bumblebee", "nickel"])
+        setup.handle_args(["-a", "-a"])
+        self.assertEquals(proc.args, ["bumblebee", "nickel", "-a"])
+
+    def test_option_e(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["-e", "progname", "arg1", "arg2"])
+        self.assertEquals(proc.cmd, "progname")
+        self.assertEquals(proc.args, ["arg1", "arg2"])
+
+    def test_option_e_2(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--prog", "progname", "-e", "arg1", "arg2"])
+        self.assertEquals(proc.cmd, "progname")
+        self.assertEquals(proc.args, ["arg1", "arg2"])
+
+    def test_option_prog(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--prog", "elephant"])
+        self.assertEquals(proc.cmd, "elephant")
+        self.assertRaises(pola_run_args.BadArgException,
+                          lambda: setup.handle_args(["--prog", "heffalump"]))
+
+    def test_option_prog_2(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--prog=elephant"])
+        self.assertEquals(proc.cmd, "elephant")
+
+    def test_option_env(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--env", "FOO=bar=qux"])
+        self.assertEquals(proc.env["FOO"], "bar=qux")
+
+    def test_option_fd(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        self.assertEquals(proc.fds, {})
+        setup.handle_args(["--fd=1"])
+        self.assertEquals(proc.fds.keys(), [1])
+
+    def test_option_f(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.caller_root = plash.env.get_root_dir()
+        temp_dir = self.make_temp_dir()
+        setup.handle_args(["-fw", temp_dir])
+        root_dir = proc.get_namespace().get_root_dir()
+        tmpdir = plash.namespace.resolve_obj(root_dir, temp_dir)
+        tmpdir.dir_create_file(os.O_WRONLY, 0666, "temp-file")
+
+    # This could be combined with the previous test using a
+    # non-deterministic choice operation.
+    def test_option_f_2(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.caller_root = plash.env.get_root_dir()
+        temp_dir = self.make_temp_dir()
+        setup.handle_args(["-fw=%s" % temp_dir])
+        root_dir = proc.get_namespace().get_root_dir()
+        tmpdir = plash.namespace.resolve_obj(root_dir, temp_dir)
+        tmpdir.dir_create_file(os.O_WRONLY, 0666, "temp-file")
+
+    def test_option_t(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.caller_root = plash.env.get_root_dir()
+        setup.handle_args(["-tw", "/foo/bar", self.make_temp_dir()])
+        root_dir = proc.get_namespace().get_root_dir()
+        tmpdir = plash.namespace.resolve_obj(root_dir, "/foo/bar")
+        tmpdir.dir_create_file(os.O_WRONLY, 0666, "temp-file")
+
+    def test_option_B(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.caller_root = plash.env.get_root_dir()
+        setup.handle_args(["-B"])
+
+    def test_default_cwd(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        self.assertEquals(setup.cwd, None)
+
+    def test_no_cwd(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--no-cwd"])
+        self.assertEquals(setup.cwd, None)
+
+    def test_copy_cwd(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.caller_root = plash.env.get_root_dir()
+        setup.handle_args(["--copy-cwd"])
+        self.assertEquals(plash.namespace.dirstack_get_path(setup.cwd),
+                          os.getcwd())
+
+    def test_set_cwd(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.caller_root = plash.env.get_root_dir()
+        temp_dir = self.make_temp_dir()
+        setup.handle_args(["--cwd=%s" % temp_dir])
+        self.assertEquals(plash.namespace.dirstack_get_path(setup.cwd),
+                          temp_dir)
+
     def test_environment_from_tmp(self):
         proc = plash.process.ProcessSpecWithNamespace()
         setup = pola_run_args.ProcessSetup(proc)
@@ -145,6 +261,36 @@ class TestEnvironment(unittest.TestCase):
         setup.handle_args(["--x11"])
         self.assertTrue("DISPLAY" not in proc.env)
         self.assertTrue("XAUTHORITY" not in proc.env)
+
+    def test_option_log_file(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--log-file=%s" % self.make_temp_file("")])
+
+    def test_option_powerbox(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--powerbox"])
+
+    def test_option_petname(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        setup.handle_args(["--pet-name=My pet goat"])
+
+    def test_invalid_options(self):
+        proc = plash.process.ProcessSpecWithNamespace()
+        setup = pola_run_args.ProcessSetup(proc)
+        for arg_list in (["-a"], ["-abumblebee"], ["-abumblebee", "-a"],
+                         ["-efoo"], ["-efoo", "bar"],
+                         ["-f"], ["-fz", "file"],
+                         ["-t"], ["-t", "path1"], ["-tz", "path1", "path2"],
+                         ["-Bfoo"],
+                         ["-hfoo"],
+                         ["--progfoo"],
+                         ["--env", "FOOBAR"],
+                         ["not_an_option"]):
+            self.assertRaises(pola_run_args.BadArgException,
+                              lambda: setup.handle_args(arg_list))
 
 
 if __name__ == '__main__':

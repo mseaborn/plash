bc8a2679a640775d32ff2e75f66e278fbbfda0d7
markseaborn <markseaborn@e2ebe5e9-4d0c-0410-8560-844d39661dae>
2008-06-08 15:28:47 +0000

    StoryTest3: format_glibc_tests:  * Add all glibc tests that are currently failing to the known_failures list.  * Add --verbose option to print contents of tests' *.out files.
    git-svn-id: svn://svn.gna.org/svn/plash/trunk@805 e2ebe5e9-4d0c-0410-8560-844d39661dae

--- plash/tests/format_glibc_tests.py
+++ plash/tests/format_glibc_tests.py
@@ -19,28 +19,109 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
 # USA.
 
+import optparse
+import os
 import re
 import sys
 
 
-known_failures = """
-intl/tst-gettext.out: tst-gettext.sh does not invoke executable via wrapper
+known_failures = (
+# Cause known.  Fixable.
+"""\
+dirent/bug-readdir1.out: possibly close(-1) not working as expected
 dirent/tst-fdopendir.out: needs fdopendir (not implemented)
 dirent/tst-fdopendir2.out: needs fdopendir (not implemented)
-io/tst-openat.out: needs fdopendir (not implemented)
-io/tst-unlinkat.out: needs fdopendir (not implemented)
+intl/tst-gettext.out: tst-gettext.sh does not invoke executable via wrapper
+io/test-lfs.out: stat64() needs fixing
+io/tst-faccessat.out: needs fdopendir (not implemented)
+io/tst-fchmodat.out: needs fdopendir (not implemented)
 io/tst-fstatat.out: needs fdopendir (not implemented)
 io/tst-futimesat.out: needs fdopendir (not implemented)
-io/tst-renameat.out: needs fdopendir (not implemented)
-io/tst-fchmodat.out: needs fdopendir (not implemented)
-io/tst-faccessat.out: needs fdopendir (not implemented)
-io/tst-symlinkat.out: needs fdopendir (not implemented)
 io/tst-linkat.out: needs fdopendir (not implemented)
-io/tst-readlinkat.out: needs fdopendir (not implemented)
 io/tst-mkdirat.out: needs fdopendir (not implemented)
-io/tst-mknodat.out: needs fdopendir (not implemented)
 io/tst-mkfifoat.out: needs fdopendir (not implemented)
+io/tst-mknodat.out: needs fdopendir (not implemented)
+io/tst-openat.out: needs fdopendir (not implemented)
+io/tst-readlinkat.out: needs fdopendir (not implemented)
+io/tst-renameat.out: needs fdopendir (not implemented)
+io/tst-symlinkat.out: needs fdopendir (not implemented)
+io/tst-unlinkat.out: needs fdopendir (not implemented)
+nptl/tst-cancel4.out: probably a problem cancelling close()
+nptl/tst-cancel5.out: probably a problem cancelling close()
+nptl/tst-cancelx4.out: probably a problem cancelling close()
+nptl/tst-cancelx5.out: probably a problem cancelling close()
 """
+
+# Needs C++
+"""\
+debug/tst-chk4: /usr/bin/ld: cannot find -lstdc++
+debug/tst-chk5: /usr/bin/ld: cannot find -lstdc++
+debug/tst-chk6: /usr/bin/ld: cannot find -lstdc++
+debug/tst-lfschk4: /usr/bin/ld: cannot find -lstdc++
+debug/tst-lfschk5: /usr/bin/ld: cannot find -lstdc++
+debug/tst-lfschk6: /usr/bin/ld: cannot find -lstdc++
+dlfcn/bug-atexit3-lib.so: /usr/bin/ld: cannot find -lstdc++
+nptl/tst-cancel24: /usr/bin/ld: cannot find -lstdc++
+"""
+
+# Cause known.  Harder to fix.
+"""\
+nptl/tst-umask1.out: umask() and mkfifo() are not supported
+posix/tst-chmod.out: errno from creat(); use of dirfd()
+posix/tst-dir.out: use of dirfd()
+"""
+
+# Probably problems caused by exec'ing subprocesses or using static
+# linking.
+"""\
+dlfcn/tststatic.out: ??
+dlfcn/tststatic2.out: ??
+elf/order.out: ??
+elf/order2.out: ??
+elf/tst-pathopt.out: ??
+elf/tst-stackguard1-static.out: ??
+elf/tst-tls1-static.out: ??
+elf/tst-tls2-static.out: ??
+elf/tst-tls9-static.out: ??
+io/bug-ftw2.out: see io/ftwtest-sh
+io/bug-ftw4.out: see io/ftwtest-sh
+io/ftwtest.out: see io/ftwtest-sh
+malloc/tst-mtrace.out: see malloc/tst-mtrace.sh
+nptl/tst-cancel7.out: nested wrapper invocation
+nptl/tst-cancelx7.out: nested wrapper invocation
+nptl/tst-exec4.out: nested wrapper invocation
+nptl/tst-locale1.out: Cannot fork test program: Function not implemented
+nptl/tst-locale2.out: Cannot fork test program: Function not implemented
+nptl/tst-stackguard1-static: duplicate symbol, can't statically link
+posix/globtest.out: see globtest.sh
+posix/tst-exec.out: ??
+posix/tst-execl2.out: ??
+posix/tst-execle2.out: ??
+posix/tst-execlp2.out: ??
+posix/tst-execv2.out: ??
+posix/tst-execve2.out: ??
+posix/tst-execvp2.out: ??
+posix/tst-spawn.out: ??
+posix/tst-vfork3-mem: ??
+posix/wordexp-tst.out: see posix/wordexp-tst.sh
+rt/tst-mqueue7.out: nested wrapper invocation
+"""
+
+# Cause unknown.
+"""\
+elf/tst-audit1.out: segfault
+elf/tst-audit2.out: segfault
+elf/tst-stackguard1.out: "stack guard canaries are not randomized enough"
+grp/testgrp.out: ??
+grp/tst_fgetgrent.out: ??
+libio/tst-fopenloc.check: ??
+localedata/mtrace-tst-leaks: ??
+login/tst-utmp.out: ??
+login/tst-utmpx.out: ??
+nptl/tst-attr3.out: pthread_getattr_np fails (does it need /proc?)
+nptl/tst-stackguard1.out: "stack guard canaries are not randomized enough"
+rt/tst-cpuclock2.out: ??
+""")
 
 known_failures_dict = dict(line.split(": ", 1)
                            for line in known_failures.strip().split("\n"))
@@ -57,11 +138,27 @@ def find_failures(stream):
             yield match.group("target")
 
 
-def main(args):
+def print_test_output(filename):
+    fh = open(filename, "r")
+    for line_number, line in enumerate(fh):
+        if line_number > 100:
+            print "(truncated)"
+            break
+        else:
+            print ">" + line.rstrip("\n")
+    fh.close()
+
+
+def main(argv):
+    parser = optparse.OptionParser("Usage: %prog <make-log>")
+    parser.add_option("-v", "--verbose", dest="verbose", default=False,
+                      action="store_true",
+                      help="Show more information about failures")
+    options, args = parser.parse_args(argv)
     if len(args) != 1:
-        print "Usage: %s <make-log>" % sys.argv[0]
-        return 2
+        parser.error("Expected log argument")
     input_file = args[0]
+    build_dir = "glibc-build"
     failures = list(find_failures(open(input_file, "r")))
     real_failure_list = [target for target in failures
                          if target not in known_failures_dict]
@@ -72,11 +169,16 @@ def main(args):
 
     def format_list(name, target_list):
         print "* %i %s" % (len(target_list), name)
-        for target in target_list:
+        for target in sorted(target_list):
             if target in known_failures_dict:
                 print "%s: %s" % (target, known_failures_dict[target])
             else:
                 print target
+            if options.verbose:
+                output_file = os.path.join(build_dir, "%s.temp" % target)
+                if os.path.exists(output_file):
+                    print_test_output(output_file)
+                print
         print
 
     format_list("unexpected failures", real_failure_list)
